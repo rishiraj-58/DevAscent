b3a093d391d1e46d280827858732b78d8f951805
rishiraj-58 <s.rishiraj2002@gmail.com>
2025-11-24 12:05:29 +0530

    Initial Commit

---
+++ DevAscent/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DevAscent
+//
+//  Created by Rishiraj on 24/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DevAscent/DevAscentApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevAscentApp.swift
+//  DevAscent
+//
+//  Created by Rishiraj on 24/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DevAscentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
